b1194e5485acf9f73dd94538effdd2bd31a22ae9
Andre <elij.richards22@gmail.com>
2015-11-27 13:44:10 -0500

    First new branch Commit

--- app/src/main/java/com/andrerichards/andre/to_dolist/MainActivity.java
+++ app/src/main/java/com/andrerichards/andre/to_dolist/MainActivity.java
@@ -1,5 +1,6 @@
 package com.andrerichards.andre.to_dolist;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
--- app/src/main/java/com/andrerichards/andre/to_dolist/MainActivityFragment.java
+++ app/src/main/java/com/andrerichards/andre/to_dolist/MainActivityFragment.java
@@ -1,6 +1,9 @@
 package com.andrerichards.andre.to_dolist;
 
+import android.app.AlertDialog;
 import android.app.ListFragment;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -8,6 +11,8 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -16,19 +21,49 @@ import java.util.List;
  * A placeholder fragment containing a simple view.
  */
 public class MainActivityFragment extends ListFragment {
-//
+
 //    ArrayList<String> toDoItems = new ArrayList<>();
 //    ArrayAdapter<String> adapter;
-    Button addButton;
+//    Button addButton;
+//    Context context;
 
     public MainActivityFragment() {
     }
 
-    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-
-        return inflater.inflate(R.layout.fragment_main, container, false);
-
+//        toDoItems.add("one");
+//        toDoItems.add("two");
+//        toDoItems.add("three");
+//        adapter = new ArrayAdapter<>(getActivity(), android.R.layout.simple_list_item_1,
+//                toDoItems);
+//        setListAdapter(adapter);
+//        addButton = (Button) container.findViewById(R.id.addButton);
+//        addButton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                switch (v.getId()){
+//                    case R.id.addButton:
+//                        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+//                        builder.setTitle("Add a New Task");
+//                        builder.setMessage("What would you like to do?");
+//                        final EditText inputField = new EditText(context);
+//                        builder.setView(inputField);
+//                        builder.setPositiveButton("Save", new DialogInterface.OnClickListener() {
+//                            @Override
+//                            public void onClick(DialogInterface dialog, int which) {
+//                                String newItemToList = inputField.getText().toString();
+//                                toDoItems.add(newItemToList);
+//                            }
+//                        });
+//                        builder.setNegativeButton("Cancel", null);
+//                        builder.create().show();
+//                }
+//            }
+//        });
+        View view = inflater.inflate(R.layout.fragment_main, container, false);
+        return view;
     }
+
+
 }
